897d6ef4a8a81acf484eb01a4ce62b7b033f6132
柳海超 <yishu_email@163.com>
2023-02-20 14:42:36 +0800

    chushihua

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="hello">
+    <div id="gundong" @scroll="scroll($event)" class="messageBox">
+      <li :key="index" v-for="(item, index) in messagelist">
+        <span v-if="item.loading">上传中</span>
+        消息{{ index }}
+      </li>
+    </div>
+    <input type="file" name="image" accept="image/*" @change="upload($event)">
+    <button @click="send()">发送</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      messagelist: Array(20).fill('1'),
+      lastScrollHeight: 0
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      let msg = document.getElementById('gundong') // 获取对象
+      this.lastScrollHeight = msg.scrollHeight
+      msg.scrollTop = msg.scrollHeight // 滚动高度
+    })
+  },
+  methods: {
+    upload(event) {
+      var e = window.event || event;
+      console.log(e)
+      // 获取当前选中的文件
+      var File = e.target.files[0];
+      console.log(File);//打印值看下面图片,简单点的话我们直接把这个数据给后台处理就可以了
+      const obj={
+        file:File,
+        type:'file',
+        loading:true
+      }
+      setTimeout(() => {
+        obj.loading=false;
+        console.log(this.messagelist);
+        this.$forceUpdate();
+      }, 10000);
+      console.log(this.messagelist);
+      this.messagelist.push(obj)
+      this.$nextTick(() => {
+        let msg = document.getElementById('gundong') // 获取对象
+        this.lastScrollHeight = msg.scrollHeight
+        msg.scrollTop = msg.scrollHeight // 滚动高度
+      })
+    },
+
+    send() {
+      console.log(3333);
+      this.messagelist = [...this.messagelist, '111']
+      this.$nextTick(() => {
+        let msg = document.getElementById('gundong') // 获取对象
+        this.lastScrollHeight = msg.scrollHeight
+        msg.scrollTop = msg.scrollHeight // 滚动高度
+      })
+    },
+    scroll(event) {
+      if (event.target.scrollTop == 0) {
+        let msg = document.getElementById('gundong')
+
+        const newdata = Array(20).fill('1');
+        this.messagelist = [...newdata, ...this.messagelist]
+
+        this.$nextTick(() => {
+          console.log(msg.scrollHeight, this.lastScrollHeight);
+          let scrollTop = msg.scrollHeight - this.lastScrollHeight
+          msg.scrollTop = scrollTop
+          this.lastScrollHeight = msg.scrollHeight
+        })
+
+
+
+      }
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+li{
+  list-style: none;
+}
+.messageBox {
+  width: 300px;
+  height: 300px;
+  border: 1px solid #000;
+  overflow: auto;
+}
+</style>
